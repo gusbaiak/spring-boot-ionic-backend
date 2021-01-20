8db27bf47a79f6dff0221a4fb9ba9a87d4f847c9
Gustavo Baiak <gustavobak15@hotmail.com>
2021-01-20 17:01:54 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/gusbak/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gusbak/cursomc/resources/CategoriaResource.java
@@ -15,19 +15,19 @@ public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
-		
+
 		Categoria cat1 = new Categoria();
 		cat1.setId(1);
 		cat1.setNome("Informática");
-		
+
 		Categoria cat2 = new Categoria();
 		cat2.setId(2);
 		cat2.setNome("Escritório");
-		
+
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
-		
+
 		return lista;
 	}
 }
